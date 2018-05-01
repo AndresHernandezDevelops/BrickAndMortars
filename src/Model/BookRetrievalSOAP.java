18f58095f2c10b040ec46820d68adda7a8fbebc5
Juyoung Kim <JYK3216@gmail.com>
2018-05-01 11:18:43 -0400

    finished implementing SOAP
    NOTE now soap uses delimiter ':' to seprate data

--- src/Model/BookRetrievalSOAP.java
+++ src/Model/BookRetrievalSOAP.java
@@ -9,7 +9,8 @@ import DAO.BookDAO;
 public class BookRetrievalSOAP {
 
 	//data from database
-	HashMap<String, BookBean> data;
+	private static HashMap<String, BookBean> data;
+	private static String delimiter = ":";
 	
 	//constructor
 	public BookRetrievalSOAP()
@@ -28,9 +29,44 @@ public class BookRetrievalSOAP {
 	}
 	
 	//gets book info from book id (required by project)
-	public BookBean getProductInfo(String productID)
+	//returns a string seprated by delimiter ':'
+	public String getProductInfo(String productID)
 	{
-		return data.get(productID);
+		BookBean tmp = data.get(productID);
+		String bid = tmp.getbID();
+		String cate = tmp.getCategory();
+		int price = tmp.getPrice();
+		
+		if (bid.contains(delimiter))
+		{
+			String[] tmpchar = bid.split(delimiter);
+			StringBuilder build = new StringBuilder();
+			build.append(tmpchar[0]);
+			int i = 1;
+			for (i = 1; i < tmpchar.length; i++)
+			{
+				build.append("\\");
+				build.append(tmpchar[i]);
+			}
+			cate = build.toString();
+		}
+		
+		if (bid.contains(delimiter))
+		{
+			String[] tmpchar = cate.split(delimiter);
+			StringBuilder build = new StringBuilder();
+			build.append(tmpchar[0]);
+			int i = 1;
+			for (i = 1; i < tmpchar.length; i++)
+			{
+				build.append("\\");
+				build.append(tmpchar[i]);
+			}
+			cate = build.toString();
+		}
+		
+		String out = bid + delimiter + cate + delimiter + price;
+		return out;
 	}
 	
 	//need to make it update the hashmap every time changes occur in the database

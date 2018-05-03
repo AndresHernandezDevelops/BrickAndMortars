43701d0d89d75739d45a95cf7e8b4f880526195f
Ju-Young Kim <jyk3216@hpc11.eecs.yorku.ca>
2018-05-02 20:50:56 -0400

    fixed the front end of the login page

--- WebContent/res/login.js
+++ WebContent/res/login.js
@@ -1,14 +1,39 @@
-/**
- * actually meant to be for register
- */
-function check_pw(){
-var ok = true;
-var pw = document.getElementById("pw").value;
-var pwconf = document.getElementById("pwconf").value;
-if (!(pwconf === pw)){
-	alert("Passwords don't match!");
-	ok = false;
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	var ok = test(username) && test(password);
+	if(!ok)
+		alert("numbers and letters only please");
+	return ok;
 }
 
-return ok;
+
+function check_pw(){
+	var ok = true;
+	var pw = document.getElementById("password").value;
+	var pwconf = document.getElementById("pwconf").value;
+	if (!(pwconf === pw)){
+		alert("Passwords don't match!");
+		ok = false;
+	}
+	return ok;
 }
+
+
+function test(input){
+	var regex = RegExp('^[0-9a-zA-Z]+$');
+	return regex.test(input);
+}
+
+
+function copy()
+{
+	var regex = RegExp('^[0-9a-zA-Z]*@[0-9a-zA-Z]*.com$');
+	var t1 = "test";
+	var t2 = "@#$";
+	var t3 = "jyk1961@hotmail.com"
+	console.log(regex.test(t1));
+	console.log(regex.test(t2));
+	console.log(regex.test(t3));
+}
+
---
+++ src/Controller/Login.java
@@ -0,0 +1,41 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/Login.jspx").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getParameter("username"));
+		System.out.println(request.getParameter("password"));
+		if(request.getParameter("registerButton")!=null)
+			System.out.println(request.getParameter("registerButton"));
+	}
+
+}

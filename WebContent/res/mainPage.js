ea92b3b3701c6caeb8f6bf5c0d597b77b8192564
Juyoung Kim <JYK3216@gmail.com>
2018-05-19 13:50:15 -0400

    updated rest client and made a function to remove scirpt injects in review

--- WebContent/res/mainPage.js
+++ WebContent/res/mainPage.js
@@ -139,3 +139,11 @@ function buildTable(result){
     
     return table;
 }
+
+function checkInject()
+{
+	var text = document.getElementById("review");
+	var textval = text.value;
+	textval = textval.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	console.log(textval);
+}

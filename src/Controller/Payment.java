f946cc6539ea86bbff13ec575e4b739e9d777ea2
Andres Hernandez <mymsong@my.yorku.ca>
2018-07-10 00:42:16 -0400

    payment now redirects to the jspx

--- src/Controller/Payment.java
+++ src/Controller/Payment.java
@@ -7,35 +7,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Bean.CartBean;
+import Bean.SetOfCartsBean;
+
 /**
  * Servlet implementation class Payment
  */
 @WebServlet("/Payment")
 public class Payment extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+	private static final long serialVersionUID = 1L; 
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Payment() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
+	public void processParameters(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String username = request.getSession().getAttribute("username").toString();
+		String fname = request.getAttribute("fname").toString(); //fix lname and fname once the jspx has a form to submit
+		String lname = request.getAttribute("lname").toString();
+		CartBean currentCart;
+		
+		if(username != null) 
+			currentCart = SetOfCartsBean.getInstance().getCartByUsername(username);
+		else
+			currentCart = SetOfCartsBean.getInstance().retrieveCart(request.getSession().getId());
+	}
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String target = "/Payment.jspx";
+		request.getRequestDispatcher(target).forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		processParameters(request, response);
 	}
 
 }
